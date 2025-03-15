f7c14220aea03104740d5e6724323b9762b2c279
Abhishek-S-A-2201 <abhishek.s.a2201@gmail.com>
2025-03-15 18:05:23 +0530

    Complete hero section

--- web_development/tailwind_css/nike_website/src/App.tsx
+++ web_development/tailwind_css/nike_website/src/App.tsx
@@ -1,11 +1,48 @@
-import "./App.css";
+import {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+} from "./sections";
 
-function App() {
+import NavBar from "./components/NavBar";
+
+const App = () => {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <main className="relative">
+                <NavBar />
+                <section className="xl:padding-l wide:padding-r padding-b">
+                    <Hero />
+                </section>
+
+                <section className="padding">
+                    <PopularProducts />
+                </section>
+                <section className="padding">
+                    <SuperQuality />
+                </section>
+                <section className="padding-x py-10">
+                    <Services />
+                </section>
+                <section className="padding">
+                    <SpecialOffer />
+                </section>
+                <section className="padding bg-pale-blue">
+                    <CustomerReviews />
+                </section>
+                <section className="padding-x sm:py-32 py-16 w-full">
+                    <Subscribe />
+                </section>
+                <section className="padding bg-black padding-x padding-t pb-8">
+                    <Footer />
+                </section>
+            </main>
         </>
     );
-}
-
+};
 export default App;
---
+++ web_development/tailwind_css/nike_website/src/components/index.ts
@@ -0,0 +1,5 @@
+import Button from "./Button";
+import NavBar from "./NavBar";
+import ShoeCard from "./ShoeCard";
+
+export { Button, NavBar, ShoeCard };
